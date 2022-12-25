12f0d9668352f71a78b01a202356dfe09132e2fd
satrio <satrionugroho091@gmail.com>
2022-12-25 13:55:28 +0700

    Header Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
+import Header from './components/Header';
 import './index.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="text-3xl font-bold underline text-red-600">Hello world!</h1>
-      </header>
+      <Header/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header id="Heder">
+      <h1 className="text-lg font-bold py-4 px-5" style={{ color: '#1E1F21' }}>
+        Product Roadmap
+      </h1>
+      <hr style={{backgroundColor: "#E0E0E0"}}/>
+    </header>
+  );
+}
+
+export default Header;
